8fe35c880c201e9c0c2e73a2e45af5fcb801c09c
fr0zen <176347124+fr0zenStore@users.noreply.github.com>
2025-03-09 21:01:32 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,7 @@
+    <footer class="container py-4 text-center">
+        <p>&copy; <?php echo date("Y"); ?> Easy TMDB - Tutti i diritti riservati.</p>
+    </footer>
 
+    <?php wp_footer(); ?>
+</body>
+</html>
