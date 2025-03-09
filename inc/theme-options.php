0c89c49c1417c3dc8662fcf317202fa0fe44293c
fr0zen <176347124+fr0zenStore@users.noreply.github.com>
2025-03-09 15:35:50 +0100

    Update theme-options.php

--- inc/theme-options.php
+++ inc/theme-options.php
@@ -1,1 +1,14 @@
+function easy_tmdb_customizer_settings($wp_customize) {
+    $wp_customize->add_section('easy_tmdb_general', [
+        'title' => 'Impostazioni Generali',
+        'priority' => 30,
+    ]);
 
+    $wp_customize->add_setting('easy_tmdb_logo');
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'easy_tmdb_logo', [
+        'label' => 'Logo',
+        'section' => 'easy_tmdb_general',
+        'settings' => 'easy_tmdb_logo',
+    ]));
+}
+add_action('customize_register', 'easy_tmdb_customizer_settings');
