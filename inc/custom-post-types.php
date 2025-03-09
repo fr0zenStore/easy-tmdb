e6d41d6df5eacde7be1ccab90fecf3aa63f19a5b
fr0zen <176347124+fr0zenStore@users.noreply.github.com>
2025-03-09 20:57:02 +0100

    Update custom-post-types.php

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -1,22 +1,20 @@
+<?php
 function easy_tmdb_register_cpt() {
-    // Movies
-    register_post_type('movie', array(
+    register_post_type('movie', [
         'label' => 'Movies',
         'public' => true,
         'menu_icon' => 'dashicons-video-alt2',
         'supports' => ['title', 'editor', 'thumbnail', 'custom-fields'],
         'rewrite' => ['slug' => 'movies'],
-        'taxonomies' => ['category', 'post_tag'],
-    ));
+    ]);
 
-    // Series
-    register_post_type('series', array(
+    register_post_type('series', [
         'label' => 'Series',
         'public' => true,
         'menu_icon' => 'dashicons-tickets-alt',
         'supports' => ['title', 'editor', 'thumbnail', 'custom-fields'],
         'rewrite' => ['slug' => 'series'],
-        'taxonomies' => ['category', 'post_tag'],
-    ));
+    ]);
 }
 add_action('init', 'easy_tmdb_register_cpt');
+?>
